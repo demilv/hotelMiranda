489f07c529799b8a5a53a1918eeba841b91ff31f
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-06-20 19:25:53 +0200

    Preparando rooms

---
+++ js/swiperRoom.js
@@ -0,0 +1,9 @@
+let slider = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    }
+});
