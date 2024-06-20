47826a7e9588bcdf89b2184f0ad0d326446d3483
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-06-20 23:55:11 +0200

    Rooms (casi) acabado

--- js/swiperRoom.js
+++ js/swiperRoom.js
@@ -1,9 +1,13 @@
-let slider = new Swiper('.swiper', {
+let slider = new Swiper('.swiperM', {
     pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper-paginationM',
         clickable: true,
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
         },
-    }
+    },
+    navigation: {
+        nextEl: '.swiper-button-nextM1',
+        prevEl: '.swiper-button-prevM1',
+    },  
 });
